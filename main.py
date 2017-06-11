4a4e16aa96463ba45e6fcbf829321878c26f574d
Eric Bailey <popcorncolonel@gmail.com>
2017-06-11 10:27:30 -0400

    Added ndb models and some comments for the handlers

--- main.py
+++ main.py
@@ -12,11 +12,39 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 )
 
 
-class Report(ndb.model):
-    pass
+class GlobalStats(ndb.Model):
+    '''
+    Global information needed for all reports (like yearly dreamcount)
+    '''
+    num_customers_this_year = ndb.IntegerProperty()
+    num_dreams_this_year = ndb.IntegerProperty()
+    daily_dream_goal = ndb.IntegerProperty()  # how many dreams would we like today?
+    year_goal = ndb.StringProperty()
+
+
+class Report(ndb.Model):
+    '''
+    Information for the daily report
+    '''
+    date = ndb.DateTimeProperty()
+
+    month_goal = ndb.StringProperty()
+
+    num_customers = ndb.IntegerProperty()
+    num_dreamers = ndb.IntegerProperty()
+    num_dreams = ndb.IntegerProperty()
+    end_time = ndb.IntegerProperty()
+    total_bowls = ndb.IntegerProperty()
+    total_cups = ndb.IntegerProperty()
+    chopsticks_missing = ndb.IntegerProperty()
+    money_off_by = ndb.IntegerProperty()
+    positive_cycle = ndb.IntegerProperty()
 
 
 class ViewAllReportsHandler(webapp2.RequestHandler):
+    '''
+    Handler to view a list of all the existing daily reports
+    '''
     def get(self):
         template_values = {}
         template = JINJA_ENVIRONMENT.get_template('reports.html')
@@ -24,6 +52,9 @@ class ViewAllReportsHandler(webapp2.RequestHandler):
 
 
 class ViewReportHandler(webapp2.RequestHandler):
+    '''
+    Handler to view details about a single report
+    '''
     def get(self, datestring):
         # get corresponding report from datestring, if it exists
         template_values = {}
@@ -32,6 +63,11 @@ class ViewReportHandler(webapp2.RequestHandler):
 
 
 class CreateReportHandler(webapp2.RequestHandler):
+    '''
+    Handler to create a new report.
+        GET this endpoint to get a create report form
+        POST here to save a new one do the db
+    '''
     def get(self):
         template_values = {}
         template = JINJA_ENVIRONMENT.get_template('createreport.html')
@@ -44,6 +80,9 @@ class CreateReportHandler(webapp2.RequestHandler):
 
 
 class MainHandler(webapp2.RequestHandler):
+    '''
+    Main page: Links to creating a report or viewing all reports, or view most recent report?
+    '''
     def get(self):
         template_values = {}
         # list of most recent reports
